5e91f9a0c6d7021da4ebc2c6a8c70fc296f01e37
yasukei <yasu_kei@icloud.com>
2020-04-08 22:31:31 +0900

    fixed missing '.'

--- dotfiles/INSTALL.bat
+++ dotfiles/INSTALL.bat
@@ -1,7 +1,7 @@
 rem ; Execute as administrator
 
 mklink %HOMEPATH%\_gvimrc %cd%\_gvimrc
-mklink %HOMEPATH%\vimrc %cd%\vimrc
+mklink %HOMEPATH%\vimrc %cd%\.vimrc
 mklink /D %HOMEPATH%\.vim %cd%\.vim
 
 mklink %HOMEPATH%\.gitconfig %cd%\.gitconfig
