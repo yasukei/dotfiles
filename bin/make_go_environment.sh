e03ee1e756a1176a9dc4851edf7d4947b01a3b1e
yasukei <yasu_kei@icloud.com>
2020-04-09 22:09:37 +0900

    update make_go_environment.sh

--- bin/make_go_environment.sh
+++ bin/make_go_environment.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 mkdir -p "$HOME"/go/src/github.com/yasukei
+
+VERSION=1.13
+OS=linux
+ARCH=amd64
+
+sudo curl https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz | tar -C /usr/local -xz
+
